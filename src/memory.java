9598285907efa29ff8911c9bfddf333483e1b5a3
sary nasser <sarynasser1@gmail.com>
2021-12-28 18:28:42 +0200

    add GetMemory function

--- src/memory.java
+++ src/memory.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class memory {
+
     public static String readFile(String Path) {
         StringBuilder fileContent = new StringBuilder();
         try {
@@ -20,6 +22,25 @@ public class memory {
         return fileContent.toString();
     }
 
+    public static HashMap<Long,String> GetMemory() throws FileNotFoundException {
+        HashMap<Long, String> addressToData = new HashMap<>();
+
+        Scanner MemoryFileScanner = new Scanner(new File("MEMORYFILE.txt"));
+        while (MemoryFileScanner.hasNextLine()) {
+            String PhysicalAddressToData = MemoryFileScanner.nextLine();
+            String[] AddressToDataSplitted = PhysicalAddressToData.split(" ", 2);
+            addressToData.put(Long.parseLong(AddressToDataSplitted[0]), AddressToDataSplitted[1]);
+        }
+        return addressToData;
+    }
 
 
+    public static String Execute() throws FileNotFoundException {
+        HashMap<Long,String> addressToData = GetMemory();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
