cdbc71a00852c3844e7682ceba2333fd3043690b
sary nasser <sarynasser1@gmail.com>
2021-12-29 13:04:00 +0200

    add mov functions

--- src/memory.java
+++ src/memory.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -65,11 +66,62 @@ public class memory {
         return VirtToPhys;
     }
 
+
+//    public static Integer VariablesVirtualToPhysical(HashMap<Integer,Integer> segmentToBase,String Virtual) {
+//            int segment = (Virtual.charAt(1) - '0');
+//            int offset = Integer.parseInt(Virtual.substring(2));
+//            System.out.println("virt "+Virtual);
+//            System.out.println("phys "+ (segmentToBase.get(segment)+offset));
+//            return segmentToBase.get(segment) + offset;
+//    }
+
+    public static String movDesVal(Integer destinationPhysical,
+                                   Integer value,
+                                    HashMap<Integer,String> addressToData){
+
+        addressToData.put(destinationPhysical,value.toString());
+        return "RESULT " + value + " STORED AT LOCATION " + destinationPhysical;
+    }
+
+
+    public static String movDesSrc(Integer destinationPhysical,
+                                   Integer srcPhysical,
+                                   HashMap<Integer,String> addressToData) {
+
+        String ValueInsideSrc = addressToData.get(srcPhysical);
+
+        addressToData.put(destinationPhysical,ValueInsideSrc);
+        return "RESULT " + ValueInsideSrc + " STORED AT LOCATION " + destinationPhysical;
+    }
+
     public static String Execute() throws FileNotFoundException {
         HashMap<Integer,String> addressToData = GetMemory();
         HashMap<Integer,Integer> segmentToBase = GetSegmentToBase();
-        ArrayList<Integer> Instructions =  VirtualMemoToPhysical(segmentToBase);
+        ArrayList<Integer> InstructionsPhysicalAddress =  VirtualMemoToPhysical(segmentToBase);
 
+        for (int i=0;i < InstructionsPhysicalAddress.size();i ++){
+            int InsPhysAdd = InstructionsPhysicalAddress.get(i);
+            String ans;
+            String[] instructionAndAddress = addressToData.get(InsPhysAdd).replaceAll(",","").split(" ");
+            //System.out.println(Arrays.toString(addressToData.get(InsPhysAdd).replaceAll(",", "").split(" ")));
+            String Instruction = instructionAndAddress[0];
+            // check the instruction
+            if(Instruction.equals("mov")){
+                Integer destinationPhysical =Integer.parseInt(instructionAndAddress[1].substring(1) );
+
+                // move Value to specific Place in the memory (mov %des val)
+                if(!instructionAndAddress[2].contains("%")){
+                    Integer value = Integer.parseInt(instructionAndAddress[2]);
+                    ans = movDesVal(destinationPhysical,value,addressToData);
+                }
+                // move value in variable to another variable (mov %dest %source)
+                else{
+                    Integer srcPhysical = Integer.parseInt(instructionAndAddress[2].substring(1) );
+                    ans = movDesSrc(destinationPhysical,srcPhysical,addressToData);
+                }
+            }
+
+        }
         return "";
     }
 
